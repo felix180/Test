9a72df34b36b5b13f2eb4d4e95df9a8e56a8b1ad
Felix Ernesto Angulo Juarez <tuka17@gmail.com>
2018-09-30 22:57:59 -0300

    Initial commit

---
+++ src/main/java/com/adn/test/controller/MotanteController.java
@@ -0,0 +1,16 @@
+package com.adn.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.Documented;
+
+@RestController
+public class MotanteController {
+
+    @RequestMapping(value = "/test",method = RequestMethod.POST)
+    public String test(){
+        return "hola";
+    }
+}
