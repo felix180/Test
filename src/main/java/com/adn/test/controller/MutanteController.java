7fcc7d17bfa035072dbc079f1bfb1eb1f4335099
Felix Ernesto Angulo Juarez <tuka17@gmail.com>
2018-10-08 15:08:10 -0300

    Se agrega documenacion de la api autogenrada

--- src/main/java/com/adn/test/controller/MutanteController.java
+++ src/main/java/com/adn/test/controller/MutanteController.java
@@ -7,6 +7,7 @@ import org.jsondoc.core.annotation.*;
 import org.jsondoc.core.pojo.ApiStage;
 import org.jsondoc.core.pojo.ApiVisibility;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,14 +26,19 @@ public class MutanteController {
     @Autowired
     private MutanteService mutanteService;
 
-    @ApiMethod
+    @Value("${jsondoc.url}")
+    private String url;
+
+    @ApiMethod(description = "Redirecciona a la pagina de documentacion de la api")
     @RequestMapping(value = "", method = RequestMethod.GET)
     public ModelAndView test() {
-        return new ModelAndView( "redirect:/jsondoc-ui.html?url=https://testmutante.herokuapp.com/jsondoc#");
+        return new ModelAndView( "redirect:/jsondoc-ui.html?url="+url+"#");
        // return new ModelAndView( "redirect:/jsondoc-ui.html?url=http://localhost:8080/jsondoc");
     }
 
-    @ApiMethod(responsestatuscode ="203 - FORBIDDEN")
+    @ApiMethod(responsestatuscode ="200 - ok",description = "Se encarga de valudacion del adn NxN")
+    @ApiErrors(apierrors={
+            @ApiError(code="403", description="Forbidden")})
     @RequestMapping(value = "/mutant/", method = RequestMethod.POST)
     public  @ApiResponseObject ResponseEntity <Boolean> esMutante(@RequestBody @ApiBodyObject ConsultaMutanteDTO consultaMutanteDTO) {
         try{
@@ -45,6 +51,8 @@ public class MutanteController {
     }
 
     @ApiMethod(description = "Consulta estadisticas")
+    @ApiErrors(apierrors={
+            @ApiError(code="403", description="Forbidden")})
     @RequestMapping(value = "/stats", method = RequestMethod.GET)
     public @ApiResponseObject
     ResponseEntity<EstadisticaDTO> estadistica() {
